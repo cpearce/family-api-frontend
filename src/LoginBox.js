ba3ad5cdc1cd494c9f4132cb52f10bab5c0f0879
Chris Pearce <chris@pearce.org.nz>
2019-10-13 16:16:55 +1300

    Make LoginBox use tables to layout fields.

--- src/LoginBox.js
+++ src/LoginBox.js
@@ -26,27 +26,37 @@ export class LoginBox extends Component {
         return (
             <div>
                 <form className="LoginBox" onSubmit={this.handleLogin}>
-                    <label>
-                        Username:
-                <input
-                            type="text"
-                            name="username"
-                            value={this.state.username}
-                            onChange={this.handleInputChange}
-                        />
-                    </label>
-                    <br />
-                    <label>
-                        Password:
-                <input
-                            type="password"
-                            name="password"
-                            value={this.state.password}
-                            onChange={this.handleInputChange}
-                        />
-                    </label>
-                    <br />
-                    <input type="submit" value="Login" />
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td><span className="field-title">Username:</span></td>
+                                <td>
+                                    <input
+                                        type="text"
+                                        name="username"
+                                        value={this.state.username}
+                                        onChange={this.handleInputChange}
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><span className="field-title">Password:</span></td>
+                                <td>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        value={this.state.password}
+                                        onChange={this.handleInputChange}
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colSpan="2">
+                                    <input type="submit" value="Login" />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </form>
             </div>
         );
